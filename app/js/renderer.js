482d06a374d6cc680cab0ce22e5e525ebdd27c72
Caio Cruz <caio.cruz@tpv-tech.com>
2020-03-30 11:43:31 -0400

    add a sample how to work with to screen using  ipc libray of electron

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,1 +1,10 @@
+
+const { ipcRenderer } = require('electron')
+
 console.log("im in a browser screen");
+
+let linkSobre = document.querySelector('#link-about')
+
+linkSobre.addEventListener('click',function(){
+	ipcRenderer.send('open-window-about');
+});
--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-
+const { ipcMain } = require('electron') 
 const { app } = require('electron');
 const { BrowserWindow } = require('electron')
 
 app.on('ready',() => {
 	console.log("Start application");
 	let mainWindow = new BrowserWindow({
-	    width: 1280,
-	    height: 720,
+		width: 1280,
+		height: 720
 	});
 	//mainWindow.loadURL('https://www.google.com');
 	mainWindow.loadURL(`file://${__dirname}/app/index.html`);
@@ -16,3 +16,12 @@ app.on('window-all-closed', () => {
 	app.quit();
 });
 
+
+ipcMain.on('open-window-about', () => {
+	let aboutWindow = new BrowserWindow({
+		width:300,
+		height: 200
+	});
+	
+	aboutWindow.loadURL(`file://${__dirname}/app/about.html`);
+});
