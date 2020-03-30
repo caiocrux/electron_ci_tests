6c61170aab1685c04a718e9e9046a81d99324d38
Caio Cruz <caio.cruz@tpv-tech.com>
2020-03-30 11:11:20 -0400

    hello world in electron with html,css and nodejs

---
+++ main.js
@@ -0,0 +1,18 @@
+
+const { app } = require('electron');
+const { BrowserWindow } = require('electron')
+
+app.on('ready',() => {
+	console.log("Start application");
+	let mainWindow = new BrowserWindow({
+	    width: 1280,
+	    height: 720,
+	});
+	//mainWindow.loadURL('https://www.google.com');
+	mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+});
+
+app.on('window-all-closed', () => {
+	app.quit();
+});
+
