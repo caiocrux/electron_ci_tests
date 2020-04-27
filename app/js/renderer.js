1e04642696b7e5789a85a4d6c9454fca9c3a4198
Caio Cruz <caio.cruz@tpv-tech.com>
2020-04-27 13:39:14 -0400

    add initial suport to mongoDB

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,22 +1,27 @@
+const { ipcRenderer } = require('electron');
+const timer = require('./timer');
+const {MongoClient} = require('mongodb');
 
-const { ipcRenderer } = require('electron')
-const timer = require('./timer')
+const uri = "mongodb://electron_mongodb:27017/electron_db";
 
+const client = new MongoClient(uri,{ useNewUrlParser: true, useUnifiedTopology: true });
 console.log("im in a browser screen");
 
 let linkSobre = document.querySelector('#link-about');
 let buttonPlay = document.querySelector('.botao-play');
 let timerHtml = document.querySelector('.tempo');
+let name = document.querySelector('.curso');
 
 linkSobre.addEventListener('click',function(){
 	ipcRenderer.send('open-window-about');
 });
 
+
 let imgs = ['img/play-button.svg', 'img/stop-button.svg'];
 let play = false;
 buttonPlay.addEventListener('click',function(){
 	if (play) {
-		timer.stop()
+		timer.stop(name.textContent);
 		play = false
 	}
 	else {
@@ -25,5 +30,4 @@ buttonPlay.addEventListener('click',function(){
 	}
 	imgs = imgs.reverse();
 	buttonPlay.src = imgs[0];
-	
 });
